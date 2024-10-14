11a75c2ee0c49d7d1d466834ebd40fb1d6ccedcf
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-10-14 14:46:24 -0500

    Update PingMaker2.py

--- PingMaker2.py
+++ PingMaker2.py
@@ -1,1 +1,44 @@
+###imports###
+import time
+import requests
+import threading
+import subprocess
 
+##Functions###
+#subprocess outputgrab function#
+def getOutput(Command):
+  temp = subprocess.Popen(Command, shell=True, stdout=subprocess.PIPE)
+  output,error = temp.communicate()
+  output = output.decode("utf-8")
+  output = output.splitlines()
+  return output
+  
+#Ping and write thread function#
+def PingandWrite(Address):
+    try:
+      Command = "ping -c 4" + Address
+      output = getOutput(Command)
+      for line in output:
+        if "% packet loss" in line:
+          pktloss = int(line.split(', ')[2].split(" ")[0][:-1])
+          if pktloss > 25:
+            with open("/home/PingMaker/PingStats"+Address+".txt", "a") as statfile:
+              errtime = time.strftime("%H:%M:%S")
+              statfile.write("\nTarget: "+target + "PacketLoss:"+str(pktloss)+ ", Time: "+errtime)
+    except:
+        with open('NotWorking.txt','a') as f:
+                f.write(Address)
+
+####open targets file#####
+ListofTargets = []
+with open("/home/PingMaker/PingMakerTargets.txt", "r") as targetFile:
+  for line in targetFile:
+    ListofTargets.append(line)
+
+####multithres ping targets and wirte to file###
+# never stop until script is canceled
+while 1==1:
+  for Address in ListofAddresses:
+    PingThread = threading.Thread(target=PingandWrite, args=(Address,))
+    PingThread.start()
+  time.sleep(2)
