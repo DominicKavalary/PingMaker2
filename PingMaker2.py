cf708fb766640505bd372eb12e4cc700260d8146
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-10-14 20:07:51 -0500

    Update PingMaker2.py

--- PingMaker2.py
+++ PingMaker2.py
@@ -1,54 +1,35 @@
+#### Setup ####
+###Imports###
 import time
-
 import requests
-
 import threading
-
 import subprocess
 
-
-
-##Functions###
-
-#subprocess outputgrab function#
-
+###Functions###
+#subprocess output grab function#
 def getOutput(Command):
-
   temp = subprocess.Popen(Command, shell=True, stdout=subprocess.PIPE)
-
   output,error = temp.communicate()
-
   output = output.decode("utf-8")
-
   output = output.splitlines()
-
   return output
 
-  
-
 #Ping and write thread function#
-
 def PingandWrite(Address):
-
 #    try:
+# replace newlines in address. if doesnt work replace them later on in the text
       Address = Address.replace("\n","")
-  
+# make the command and get its output
       Command = "ping -c 4 " + Address
-
       output = getOutput(Command)
-
+# for every line in that output, grab the packetloss
       for line in output:
-
         if "% packet loss" in line:
-
           pktloss = int(line.split(', ')[2].split(" ")[0][:-1])
-
+# if packet loss is greater than 25%, write the timestamp and packet loss to a text file
           if pktloss > 25:
-
             with open("/home/PingMaker/PingStats"+Address+".txt", "a") as statfile:
-
               errtime = time.strftime("%D:%H:%M:%S")
-
               statfile.write("\nTarget: "+Address + " PacketLoss:"+str(pktloss)+ ", Time: "+errtime)
 
     #except:
@@ -59,30 +40,17 @@ def PingandWrite(Address):
 
  #       f.write(Address)
 
-
-
-####open targets file#####
-
+#### MAIN ####
+###open targets file and create list of targets###
 ListofTargets = []
-
 with open("/home/PingMaker/PingMakerTargets.txt", "r") as targetFile:
-
   for line in targetFile:
-
     ListofTargets=line.split(",")
 
-
-
-####multithres ping targets and wirte to file###
-
-# never stop until script is canceled
-
+####multithread ping targets###
+#never stop until script is canceled, sleep for 5 seconds before doing another thread
 while 1==1:
-
   for Address in ListofTargets:
-
     PingThread = threading.Thread(target=PingandWrite, args=(Address,))
-
     PingThread.start()
-
   time.sleep(5)
